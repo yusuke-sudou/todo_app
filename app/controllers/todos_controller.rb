4c60a2286a85d51d2b2b8d198d8d3babc8272b0a
yusuke-sudou <sudoukky22@gmail.com>
2024-01-08 14:33:53 +0900

    [Add]アクション記述

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,2 +1,25 @@
 class TodosController < ApplicationController
+    def index
+    end
+
+    def new
+    end
+
+    def create
+    end
+
+    def edit
+    end
+
+    def update
+    end
+
+    def destroy
+    end
+
+    private
+        def todo_params
+            params.require(:todo).permit(:title)
+        end
+        
 end
