24d63746eeaf68810dbe62b63a0ee0e9e6cf25cd
yusuke-sudou <sudoukky22@gmail.com>
2024-01-09 17:20:51 +0900

    [Add]index＆new

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,11 +1,16 @@
 class TodosController < ApplicationController
     def index
+        @todos = Todo.all
     end
 
     def new
+        @todo = Todo.new
     end
 
     def create
+        @todo = Todo.create(todo_params)
+        @todo.save
+        redirect_to todos_path
     end
 
     def edit
